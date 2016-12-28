64c5f32fca41d23aedba4650bbc4173164c1f7bd
Rachel Munoz <rachelumunoz@gmail.com>
2016-12-27 21:32:16 -0800

    Fixes redirect_uri route

--- app.js
+++ app.js
@@ -4,27 +4,38 @@ const http = require('http');
 const express = require('express')
 const api = require('instagram-node').instagram()
 const app = express()
-
+const session = require('express-session')
 
 api.use({
-  client_id: '6ed838ccc31e433982dd0db2fc435a46',
-  client_secret: '6d42358538ee4b21a48ab7dd113d12eb'
+  client_id: process.env.client_id,
+  client_secret: process.env.client_secret
 })
 
+app.set('trust proxy', 1)
+app.use(session({
+  secret: process.env.sessionSecret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure : false}
+}))
 
-const redirect_uri = 'http://127.0.0.1/handleauth';
+
+const redirect_uri = 'http://127.0.0.1:3000/handleauth';
 
 exports.authorize_user = function(req, res) {
   res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
 };
  
+ //need templates
 exports.handleauth = function(req, res) {
+   
   api.authorize_user(req.query.code, redirect_uri, function(err, result) {
     if (err) {
       console.log(err.body);
       res.send("Didn't work");
     } else {
-      console.log('Yay! Access token is ' + result.access_token);
+      // console.log('Yay! Access token is ' + result.access_token);
+      req.session.access_token = result.access_token
       res.send('You made it!!');
     }
   });
@@ -34,8 +45,12 @@ app.get('/authorize_user', exports.authorize_user);
 // This is your redirect URI 
 app.get('/handleauth', exports.handleauth);
  
+app.get('/', function(req, res){
+  console.log(req.session)
+  res.send('hello')
+})
 
-app.get('/', )
+
 
 
 app.set('port', process.env.PORT || 3000);
